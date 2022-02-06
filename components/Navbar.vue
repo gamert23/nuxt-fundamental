4c5d31a0fcdafb93b8e51a546baf7a7cc9f0b7fd
gamert23 <ratchanongame@gmail.com>
2022-02-06 19:08:18 +0700

    Create profile page

---
+++ components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <nuxt-link to="/"> <NuxtLogo /> </nuxt-link>
+        <nuxt-link class="nav-link" to="/"> Home </nuxt-link>
+        <nuxt-link class="nav-link" to="/blogs"> Blogs </nuxt-link>
+
+        <b-nav-item-dropdown text="More" right>
+          <nuxt-link class="dropdown-item" to="/about"> About </nuxt-link>
+          <nuxt-link class="dropdown-item" to="/profile"> Profile </nuxt-link>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <nuxt-link class="nav-link login-btn" to="/login"> Login </nuxt-link>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 15px;
+  justify-content: space-between;
+}
+.nuxt-logo {
+  width: 32px;
+  height: 35px;
+}
+
+.login-btn {
+  width: 100px;
+  text-align: center;
+  background: #00dc81;
+  border-radius: 5px;
+  color: #ddd;
+}
+
+.login-btn:hover {
+  opacity: 0.8;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,78 @@
+<template>
+  <b-container>
+    <div class="profile-block">
+      <h2 class="title">My Profile</h2>
+
+      <img v-if="user" class="profile-img" :src="user.avatar_url" />
+    </div>
+
+    <div class="repos-block" v-if="repos.length > 0">
+      <b-row>
+        <b-col cols="4" v-for="repo in repos" :key="repo.id">
+          <b-card
+            @click="redirect(repo.html_url)"
+            :title="repo.name"
+            img-src="https://picsum.photos/600/300/"
+            img-alt="Image"
+            img-top
+          />
+        </b-col>
+      </b-row>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const baseUrl = 'https://api.github.com'
+
+    const user = await $axios.$get(`${baseUrl}/users/gamert23`)
+    const repos = await $axios.$get(`${baseUrl}/users/gamert23/repos`)
+
+    return {
+      user,
+      repos,
+    }
+  },
+  data() {
+    return {
+      show: 6,
+    }
+  },
+  methods: {
+    redirect(url) {
+      window.open(url, '_blank')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.profile-block {
+  padding: 30px 0 50px 0;
+  text-align: center;
+}
+
+.title {
+  font-size: 40px;
+  margin-bottom: 30px;
+}
+
+.profile-img {
+  border-radius: 50%;
+  width: 200px;
+  border: 1px solid #ddd;
+}
+
+.card {
+  margin-bottom: 30px;
+  cursor: pointer;
+}
+
+.card-title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
